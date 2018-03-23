ce04f26d72f31d4b097cf07e39c0a2bc32fc9d30
sinwailam193 <aaronsinwailam@gmail.com>
2018-03-23 11:21:06 -0700

    Added eslint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './src/app';
+import { AppRegistry } from "react-native";
+import App from "./src/app";
 
-AppRegistry.registerComponent('react_native', () => App);
+AppRegistry.registerComponent("react_native", () => App);
